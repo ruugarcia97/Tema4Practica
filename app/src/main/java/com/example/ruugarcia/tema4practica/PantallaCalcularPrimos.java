c2cdceb738955834a64c4c96428617cfe284bd86
ruugarcia97 <37157695+ruugarcia97@users.noreply.github.com>
2019-01-23 12:02:50 +0100

    Esta es la activity del primer botón, en ella se nos muestra un texfield y un botón. En la clase java le hemos dado la funcionalidad al botón, para que cuando introduzca el número, este al ser pulsado te diga si es primo o no.

---
+++ app/src/main/java/com/example/ruugarcia/tema4practica/PantallaCalcularPrimos.java
@@ -0,0 +1,60 @@
+package com.example.ruugarcia.tema4practica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class PantallaCalcularPrimos extends AppCompatActivity {
+
+    private EditText txtResultado;
+    private Button btnComprobar;
+    private TextView lblResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcular);
+
+        setTitle("Números primos");
+
+
+        txtResultado = findViewById(R.id.txtResultado);
+        btnComprobar = findViewById(R.id.btnComprobar);
+        lblResultado = findViewById(R.id.lblResultado);
+
+        btnComprobar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!txtResultado.getText().toString().isEmpty()) {
+                    if (esPrimo(Integer.parseInt(txtResultado.getText().toString()))) {
+                        lblResultado.setText("Es primo");
+                    } else {
+                        lblResultado.setText("No es primo");
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean esPrimo(int numero){
+        boolean resultado = true;
+        int sqrtNumero = (int) Math.floor(Math.sqrt(numero));
+        if ((numero & 1) > 0) { // bitwise opeator para saber si es impar
+            for (int i = 3; i <= sqrtNumero; i+=2) { // iteramos sobre los impares
+                if (numero%i == 0){
+                    resultado = false;
+                    break;
+                }
+            }
+        } else {
+            resultado = false; // pues es par
+        }
+        return resultado;
+    }
+
+}
+
+
